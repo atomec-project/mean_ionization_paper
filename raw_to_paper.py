3af21889168b65dfe7f04563624bb79097c8485b
Timothy Callow <t.callow@hzdr.de>
2022-03-09 16:51:13 +0100

    More changes to conversion script

--- raw_to_paper.py
+++ raw_to_paper.py
@@ -12,11 +12,11 @@ with open("supp_info_for_paper.tex", "w") as f:
 
     for i, line in enumerate(lines):
         if i == 0:
-            linenew = "\\documentclass[9pt]{article}" + "\n"
+            linenew = "\\documentclass[preprint,aps]{revtex4-2}" + "\n"
             f.write(linenew)
             print(line)
         elif i == 157:
-            linenew = "\\title{Supplementary Information}" + "\n"
+            linenew = "\\title{Supplementary Information} \n"
             print(line)
             f.write(linenew)
         elif i == 317:
@@ -24,7 +24,45 @@ with open("supp_info_for_paper.tex", "w") as f:
             print(line)
             f.write(line)
         elif "Add a bibliography block to the postdoc" in line:
-            linenew = "\\bibliography{main}"
+            linenew = "\\bibliographystyle{unsrt} \n \\bibliography{main}"
             f.write(linenew)
+        elif "supp_info_9_0.png" in line:
+            linenew = " \n".join(
+                [
+                    "\\begin{figure}",
+                    "\\includegraphics{figures/density_example.pdf}",
+                    "\\label{Al:dens_example}",
+                    "\\caption{Radial density distribution for Aluminium}",
+                    "\\end{figure}",
+                ]
+            )
+            f.write(linenew)
+            print(linenew)
+
+        elif "supp_info_25_0.png" in line:
+            linenew = " \n".join(
+                [
+                    "\\begin{figure}",
+                    "\\includegraphics{figures/Carbon_DOS_comp.pdf}",
+                    "\\label{C:dos_example}",
+                    "\\caption{Radial density distribution for Aluminium}",
+                    "\\end{figure}",
+                ]
+            )
+            f.write(linenew)
+            print(linenew)
+
+        elif "supp_info_31_0.png" in line:
+            linenew = " \n".join(
+                [
+                    "\\begin{figure}",
+                    "\\includegraphics{figures/ELF_example.pdf}",
+                    "\\label{Al:ELF_example}",
+                    "\\caption{Radial density distribution for Aluminium}",
+                    "\\end{figure}",
+                ]
+            )
+            f.write(linenew)
+            print(linenew)
         else:
             f.write(line)
